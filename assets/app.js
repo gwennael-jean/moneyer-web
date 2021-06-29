85cf5606c0e7b5b639f02ca1e47d82e21d107385
Gwennael Jean <gwennael.jean@digiwin.fr>
2021-06-29 20:40:39 +0200

    feat(asset): installation de bootstrap

--- assets/app.js
+++ assets/app.js
@@ -5,8 +5,10 @@
  * (and its CSS file) in your base layout (base.html.twig).
  */
 
+import 'bootstrap'
+
 // any CSS you import will output into a single css file (app.scss in this case)
-import './styles/app.scss';
+import './styles/app.scss'
 
 // start the Stimulus application
-import './bootstrap';
+import './bootstrap'
