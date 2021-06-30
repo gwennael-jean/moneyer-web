f709d22e69c11fc0e3885638bda0a7d770acc75b
Gwennael Jean <gwennael.jean@digiwin.fr>
2021-06-30 10:09:37 +0200

    feat(sonata-block): préparation du block app.topbar.block

---
+++ src/Service/Block/TopbarBlock.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Service\Block;
+
+use Sonata\BlockBundle\Block\BlockContextInterface;
+use Sonata\BlockBundle\Block\Service\BlockServiceInterface;
+use Sonata\BlockBundle\Model\BlockInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Twig\Environment;
+
+class TopbarBlock implements BlockServiceInterface
+{
+    public function __construct(
+        private Environment $twig,
+    )
+    {
+    }
+
+    public function execute(BlockContextInterface $blockContext, ?Response $response = null): Response
+    {
+        $content = $this->twig->render($blockContext->getTemplate());
+        $response->setContent($content);
+        return $response;
+    }
+
+    public function load(BlockInterface $block): void
+    {
+        // TODO: Implement load() method.
+    }
+
+    public function getCacheKeys(BlockInterface $block): array
+    {
+        // TODO: Implement getCacheKeys() method.
+    }
+
+    public function configureSettings(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'user' => null,
+            'template' => 'blocks/topbar.html.twig',
+        ]);
+    }
+
+}
