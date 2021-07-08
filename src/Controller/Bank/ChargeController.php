026a79984666c312466e5c205aa2b6adf838a933
Gwennael Jean <gwennael.jean@digiwin.fr>
2021-07-08 17:49:24 +0200

    feat(charge): création de la page de modification

--- src/Controller/Bank/ChargeController.php
+++ src/Controller/Bank/ChargeController.php
@@ -5,7 +5,9 @@ namespace App\Controller\Bank;
 use App\Entity\Bank\Account;
 use App\Entity\Bank\Charge;
 use App\Form\Bank\ChargeType;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\FormInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,9 +15,12 @@ use Symfony\Component\Routing\Annotation\Route;
 class ChargeController extends AbstractController
 {
     #[Route('/bank/account/{account}/charge/add', name: 'bank_charge_add')]
-    public function add(Request $request, Account $account): Response
+    #[Route('/bank/account/{account}/charge/{charge}/update', name: 'bank_charge_update')]
+    #[ParamConverter('account', options: ['mapping' => ['account' => 'id']])]
+    #[ParamConverter('charge', options: ['mapping' => ['charge' => 'id']])]
+    public function add(Request $request, Account $account, ?Charge $charge = null): Response
     {
-        $charge = (new Charge())
+        $charge = $charge ?? (new Charge())
             ->setAccount($account);
 
         $form = $this->createForm(ChargeType::class, $charge);
@@ -23,15 +28,22 @@ class ChargeController extends AbstractController
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            $this->addFlash('success', $charge->getId()
+                ? "Charge modified successfully."
+                : "Charge added successfully.");
+
             $this->getDoctrine()->getManager()->persist($charge);
             $this->getDoctrine()->getManager()->flush();
 
-            $this->addFlash('success', "Charge added successfully.");
             return $this->redirectToRoute('dashboard');
         }
 
-        return $this->render('pages/bank/charge/index.html.twig', [
-            'account' => $account,
+        $template = $charge->getId()
+            ? "pages/bank/charge/update.html.twig"
+            : "pages/bank/charge/add.html.twig";
+
+        return $this->render($template, [
+            'charge' => $charge,
             'form' => $form->createView(),
         ]);
     }
