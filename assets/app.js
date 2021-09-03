8ae6c455256b33e0872689dfd492a74317cfc562
gwennael <gwennael.jean@digiwin.fr>
2021-09-03 15:22:43 +0200

    feat(vue): installation de Vue 3

--- assets/app.js
+++ assets/app.js
@@ -10,5 +10,8 @@ import 'bootstrap'
 // any CSS you import will output into a single css file (app.scss in this case)
 import './styles/app.scss'
 
-// start the Stimulus application
-import './bootstrap'
+import {createApp} from "vue";
+
+const app = createApp({
+    template: '<h1>Hello !</h1>'
+}).mount('#app');
