fe306d60c022052158d075cd6ab76eeab1c97ab6
gwennael <gwennael.jean@digiwin.fr>
2021-09-15 21:41:36 +0200

    feat(vue): installation de element-plus

--- assets/dashboard.js
+++ assets/dashboard.js
@@ -1,12 +1,16 @@
 import 'bootstrap'
 
-import {createApp} from 'vue';
+import {createApp} from 'vue'
 
 import axios from 'axios'
 import VueAxios from 'vue-axios'
 
-import App from './controllers/Dashboard';
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+
+import App from './controllers/Dashboard'
 
 createApp(App)
+    .use(ElementPlus)
     .use(VueAxios, axios)
-    .mount('#app');
+    .mount('#app')
