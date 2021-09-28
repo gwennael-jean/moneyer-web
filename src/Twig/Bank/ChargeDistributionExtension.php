fd84e45025105c15f6419cdf35ef1d78396829dc
gwennael <gwennael.jean@digiwin.fr>
2021-09-28 20:18:24 +0200

    feat(charge distribution): gestion de la distribution en modal

---
+++ src/Twig/Bank/ChargeDistributionExtension.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Twig\Bank;
+
+use App\DBAL\Types\Bank\ChargeDistributionType;
+use App\Entity\Bank\ChargeDistribution;
+use App\Service\Provider\Bank\ResourceProvider;
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
+use Twig\TwigFunction;
+
+class ChargeDistributionExtension extends AbstractExtension
+{
+    public function getFilters(): array
+    {
+        return [
+            new TwigFilter('icon_class', [$this, 'getIconClass']),
+        ];
+    }
+
+    public function getFunctions(): array
+    {
+        return [
+            new TwigFunction('icon_class', [$this, 'getIconClass']),
+        ];
+    }
+
+    public function getIconClass(ChargeDistribution $chargeDistribution)
+    {
+        switch ($chargeDistribution->getType()) {
+            case ChargeDistributionType::VIEW: return "fa-eye";
+            case ChargeDistributionType::FIFTY_FIFTY: return "fa-divide";
+            case ChargeDistributionType::RESOURCE_PERCENT: return "fa-percent";
+        }
+
+        return "fa-ban";
+    }
+}
